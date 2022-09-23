6c413004002a088e9f13a1e85c263816d2649ea8
jessiefff <jiefangli0605@gmail.com>
2022-09-23 15:06:19 -0400

    add user table

--- db/migrate/20220916192756_create_events.rb
+++ db/migrate/20220916192756_create_events.rb
@@ -2,7 +2,12 @@ class CreateEvents < ActiveRecord::Migration[7.0]
   def change
     create_table :events do |t|
       t.text :content
+      t.integer :event_id
       t.integer :user_id
+      t.datetime :start_time
+      t.datetime :end_time
+      t.text :location
+      t.text :category
 
       t.timestamps
     end
--- db/schema.rb
+++ db/schema.rb
@@ -10,13 +10,27 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.0].define(version: 2022_09_16_192756) do
+ActiveRecord::Schema[7.0].define(version: 2022_09_23_183214) do
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
 
   create_table "events", force: :cascade do |t|
     t.text "content"
+    t.integer "event_id"
     t.integer "user_id"
+    t.datetime "start_time"
+    t.datetime "end_time"
+    t.text "location"
+    t.text "category"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
+
+  create_table "users", force: :cascade do |t|
+    t.string "first_name"
+    t.string "last_name"
+    t.integer "user_id"
+    t.string "email"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
   end
