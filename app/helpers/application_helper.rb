307d69dadb89ad04e81c2017d62374d62a51c4f9
lhaodev <haolu1125@gmail.com>
2022-11-24 22:34:09 -0500

    fix calendar creator

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -3,4 +3,21 @@ module ApplicationHelper
     c = @calendar.user_calendars.where(user_id: Current.user.id)
     c.first.creator
   end
+
+  def creatorname(_calendar)
+    u = @calendar.user_id
+    user = User.find_by(id: u)
+    user.first_name + ' ' + user.last_name
+  end
+
+
+
+    def creatornamefromuser(_user_calendar)
+      c = @user_calendar.calendar_id
+      calendar = Calendar.find_by(id: c)
+    u = calendar.user_id
+    user = User.find_by(id: u)
+    user.first_name + ' ' + user.last_name
+  end
+
 end
