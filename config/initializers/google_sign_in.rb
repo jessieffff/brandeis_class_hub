bc81d8754947f552e80f027cc14c3d07bf187aa5
jessiefff <jiefangli0605@gmail.com>
2022-11-28 17:19:35 -0500

    update client

--- config/initializers/google_sign_in.rb
+++ config/initializers/google_sign_in.rb
@@ -1,4 +1,4 @@
 Rails.application.configure do
-  config.google_sign_in.client_id     = '68447613915-nu7rjhb7becumqmbmhdn9kcs20iugf7e.apps.googleusercontent.com'
-  config.google_sign_in.client_secret = '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'
+  config.google_sign_in.client_id     = '106869594984-i0da5e91783kpt3nmmnkbkf72dbnujul.apps.googleusercontent.com'
+  config.google_sign_in.client_secret = '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'
 end
