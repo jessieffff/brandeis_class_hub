d1b60f63bba8ae63398938b7a34f92b28ecf291c
lhaodev <haolu1125@gmail.com>
2022-11-28 21:23:28 -0500

    subscribe calendar link

--- app/javascript/custom/subscribe_link.js
+++ app/javascript/custom/subscribe_link.js
@@ -1,9 +1,6 @@
 document.addEventListener("turbo:load", function () {
-
-    let new_event = document.querySelector("#test");
-    console.log(new_event)
-    new_event.addEventListener("click", function(new_event) {
+    let createalink = document.querySelector("#subscribe");
+    createalink.addEventListener("click", function(createalink) {
     window.location.href = document.querySelector("textarea").value;
-    console.log(new_event)
    });
 });
