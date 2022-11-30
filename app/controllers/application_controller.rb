15d012d855ccb6f47919d41ba91484c6721a7977
jessiefff <jiefangli0605@gmail.com>
2022-11-30 15:29:11 -0500

    render 404 page

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -15,4 +15,13 @@ class ApplicationController < ActionController::Base
       redirect_to login_url, status: :see_other
     end
   end
+
+  rescue_from ActiveRecord::RecordNotFound,    with: :route_not_found
+  rescue_from ActionController::RoutingError,  with: :route_not_found
+  rescue_from ActionController::UnknownFormat, with: :route_not_found
+
+
+  def route_not_found
+    render file: Rails.public_path.join('404.html'), status: :not_found, layout: false
+  end
 end
