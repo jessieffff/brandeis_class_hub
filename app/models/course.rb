477c8a657f4304d06b24d373d5be3ebabeff0cb1
jessiefff <jiefangli0605@gmail.com>
2022-11-30 14:26:43 -0500

    delete unessary function

--- app/models/course.rb
+++ app/models/course.rb
@@ -10,33 +10,4 @@ class Course < ApplicationRecord
     foreign_key: 'course_id', # name of column containing FK in other table
     dependent: :destroy_async
   )
-
-  def self.date_convertor(date_string)
-    date = ""
-    if date_string[1] == '/' && date_string[3] == "/"
-      date <<  date_string[2]
-      date <<  "/"
-      date <<  date_string[0]
-      date <<  "/"
-      date <<  date_string[4..5]
-    elsif date_string[1] == "/" && date_string[4] == "/"
-       date <<  date_string[2..3]
-       date <<  "/"
-       date <<  date_string[0]
-       date <<  "/"
-       date <<  date_string[5..6]
-    elsif date_string[2] == "/" && date_string[4] == "/"
-      date <<  date_string[3]
-      date <<  "/"
-      date <<  date_string[0..1]
-      date <<  "/"
-      date <<  date_string[5..6]
-    elsif date_string[2] == "/" && date_string[5] == "/"
-      date <<  date_string[3..4]
-      date <<  "/"
-      date <<  date_string[0..1]
-      date <<  "/"
-      date <<  date_string[6..7]
-    end
-  end
 end
