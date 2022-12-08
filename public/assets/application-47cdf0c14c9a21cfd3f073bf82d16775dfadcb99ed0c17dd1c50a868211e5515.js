b1e7ffe454e8e3548409b934ceea71b2da48330c
duckduckgoose01 <davidshapiro@brandeis.edu>
2022-12-08 14:13:19 -0500

    render assets

---
+++ public/assets/application-47cdf0c14c9a21cfd3f073bf82d16775dfadcb99ed0c17dd1c50a868211e5515.js
@@ -0,0 +1,7 @@
+// Configure your import map in config/importmap.rb. Read more: https://github.com/rails/importmap-rails
+import "@hotwired/turbo-rails"
+import "controllers"
+import "custom/menu"
+import "custom/new_event"
+import "custom/subscribe_link"
+//= require bootstrap-datepicker;
---
+++ public/assets/custom/subscribe_link-99b9f236c6598948ca0c5db17bfef03b568c45f7d7f49c837c97542d9b01a08d.js
@@ -0,0 +1,6 @@
+document.addEventListener("turbo:load", function () {
+    let createalink = document.querySelector("#subscribe");
+    createalink.addEventListener("click", function(createalink) {
+    window.location.href = document.querySelector("textarea").value;
+   });
+});
